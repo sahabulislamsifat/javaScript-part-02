dfa1e62c422a8812d9432f8a573f33517403435a
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-18 02:46:00 +0600

    array methods

---
+++ arrayMethod.js
@@ -0,0 +1,54 @@
+// const fruits = ["Mango", "Apple", "Banana", "Pineapple", "jackfruits"];
+
+// const convertString = fruits.toString();
+// const convertString = fruits.join(" * ");
+
+// console.log(convertString);
+
+// Popping and push
+// const pop = fruits.pop();
+// const push = fruits.push("Water Melon");
+
+// console.log(fruits);
+// console.log(fruits.length);
+
+// Shift and Unshift
+// const shift = fruits.shift();
+// const unshift = fruits.unshift("Orange");
+
+// console.log(fruits);
+
+// Array Changing Elements
+// const fruits = ["Mango", "Apple", "Banana", "Pineapple", "jackfruits"];
+
+// delete fruits[1];
+
+// console.log(fruits);
+
+// Splice
+// const fruits = ["Mango", "Apple", "Banana", "Pineapple", "jackfruits"];
+
+// const splice = fruits.splice(2, 0, "Water melon", "coconut");
+// const splice = fruits.splice(2, 3, "Water melon", "coconut");
+// const spliceRemove = fruits.splice(0, 1);
+// console.log(fruits);
+
+// array marge
+// const names = "Hello, I am Sifat";
+// const age = " I am 22 years old.";
+
+// const concat = names.concat(
+//   age,
+//   "I am a Student",
+//   "I want to be a full stack engineer."
+// );
+
+// console.log(concat);
+
+// Array Slice
+const fruits = ["Mango", "Apple", "Banana", "Pineapple", "jackfruits"];
+
+const slice = fruits.slice(1, 3);
+
+console.log(fruits);
+console.log(slice);
