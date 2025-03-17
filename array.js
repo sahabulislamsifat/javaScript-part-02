bd2badd93c84033ed6019ce2d23124af0bf73a05
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-18 01:26:16 +0600

    explore array

---
+++ array.js
@@ -0,0 +1,56 @@
+// const cars = ["Saab", "Volvo", "BMW"];
+// document.getElementById("demo").innerHTML = cars;
+
+// console.log(typeof cars);
+
+// const fruits = ["Mango", "Apple", "Banana", "Pineapple", "jackfruits"];
+
+// fruits[5] = "Orange";
+// fruits[6] = "Orangee";
+// fruits[7] = "Orangeee";
+
+// console.log(fruits[4]);
+// console.log(fruits[fruits.length - 1]);
+
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+
+// function myFunction(value) {
+//   console.log(value);
+// }
+
+// fruits.forEach(myFunction);
+
+// fruits[12] = "Water Melon";
+
+// console.log(fruits);
+
+// Associative Array
+const person = {
+  name: "Sifat",
+  profession: "Student",
+  age: 22,
+};
+
+// console.log(person);
+
+const fruits = ["Mango", "Apple", "Banana", "Pineapple", "jackfruits"];
+
+// console.log(typeof person);
+// console.log(typeof fruits);
+
+// console.log(Array.isArray(person));
+// console.log(Array.isArray(fruits));
+
+// console.log(person instanceof Array);
+// console.log(fruits instanceof Array);
+
+// console.log(person instanceof Object);
+// console.log(fruits instanceof Object);
+
+// console.log(fruits[4]);
+// console.log(fruits.indexOf[4]);
+// console.log(fruits[fruits.indexOf("jackfruits")]);
+// console.log(fruits[fruits.length]);
+console.log(fruits[4]);
